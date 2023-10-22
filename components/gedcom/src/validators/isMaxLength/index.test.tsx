c4fecc7ab8e9a189c81556bcb6e5db65e7e61728
Vitaliy Charnou <graffov87@gmail.com>
2023-10-22 22:26:27 +0300

    feat(1): fix test

--- components/gedcom/src/validators/isMaxLength/index.test.tsx
+++ components/gedcom/src/validators/isMaxLength/index.test.tsx
@@ -3,21 +3,17 @@ import { describe, it, expect } from "vitest";
 import func from ".";
 
 describe("isMaxLength", () => {
-  it("Should be true if string is longer than 255 symbols", () => {
+  it("Should be false if string is longer than 255 symbols", () => {
     const string = "#".repeat(256);
-    expect(func(string)).toBeTruthy();
-  });
-
-  it("Should be false if string is equal 255 symbols", () => {
-    const string = "#".repeat(255);
     expect(func(string)).toBeFalsy();
   });
 
-  it("Should be false if string is shorter than 255 symbols", () => {
-    expect(func("test")).toBeFalsy();
+  it("Should be true if string is equal 255 symbols", () => {
+    const string = "#".repeat(255);
+    expect(func(string)).toBeTruthy();
   });
 
-  it("Should be false if string is empty", () => {
-    expect(func("")).toBeFalsy();
+  it("Should be true if string is shorter than 255 symbols", () => {
+    expect(func("test")).toBeTruthy();
   });
 });
