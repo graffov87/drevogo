040919cc5d289d7b04c56007b7d0b4f3d09099f1
Vitaliy Charnou <graffov87@gmail.com>
2023-10-23 12:52:34 +0300

    feat(1): fix return

--- components/gedcom/src/isValid.ts
+++ components/gedcom/src/isValid.ts
@@ -2,15 +2,12 @@ import * as validators from "./validators";
 
 export default (line: string): boolean => {
   const listOfValidators = ["isNotEmpty", "isMaxLength", "isFirstNumber"];
-  let isValid: boolean = true;
+  //let isValid: boolean = true;
 
   for (let i = 0; i < listOfValidators.length; i++) {
     const callValidator = listOfValidators[i];
-    if (validators[callValidator as keyof typeof validators](line) == false) {
-      isValid = false;
-      break;
-    }
+    if (validators[callValidator as keyof typeof validators](line) == false) return false;
   }
 
-  return isValid;
+  return true;
 };
